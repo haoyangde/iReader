7b38b31a7b6a3b941c01440071042616023c5dea
zzyong <>
2018-08-23 09:37:29 +0800

    add debug log

--- iReader/iReader/App/AppDelegate.m
+++ iReader/iReader/App/AppDelegate.m
@@ -27,6 +27,7 @@
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
+    IRDebugLog(@"");
     return YES;
 }
 
@@ -39,6 +40,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+    IRDebugLog(@"");
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
@@ -46,12 +48,14 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
+    IRDebugLog(@"");
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
+    IRDebugLog(@"");
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
