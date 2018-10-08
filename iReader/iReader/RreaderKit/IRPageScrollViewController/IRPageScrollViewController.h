da96a68344df7eef5374d3885c52da3bffd4231d
zzyong <zouzhiyong@zzyong.local>
2018-10-08 17:56:50 +0800

    IRPageScrollViewController init

---
+++ iReader/iReader/RreaderKit/IRPageScrollViewController/IRPageScrollViewController.h
@@ -0,0 +1,13 @@
+//
+//  IRPageScrollViewController.h
+//  iReader
+//
+//  Created by zzyong on 2018/10/8.
+//  Copyright © 2018年 zouzhiyong. All rights reserved.
+//
+
+#import "IRBaseViewController.h"
+
+@interface IRPageScrollViewController : IRBaseViewController
+
+@end
---
+++ iReader/iReader/RreaderKit/IRPageScrollViewController/IRPageScrollViewController.m
@@ -0,0 +1,23 @@
+//
+//  IRPageScrollViewController.m
+//  iReader
+//
+//  Created by zzyong on 2018/10/8.
+//  Copyright © 2018年 zouzhiyong. All rights reserved.
+//
+
+#import "IRPageScrollViewController.h"
+
+@interface IRPageScrollViewController ()
+
+@end
+
+@implementation IRPageScrollViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+@end
